83bb86f9d983193d011619a0fca9aa1514b414bb
Felipe Graciano <22282790@cfp127aluno.local>
2023-03-20 16:00:25 -0300

    front-construido

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const mapa = document.querySelector('svg')
+
+const getEstados = (event) =>{
+
+    const estado = event.target.id.replace('BR-', '')
+    console.log(estado)
+}
+
+mapa.addEventListener('click', getEstados)
+
